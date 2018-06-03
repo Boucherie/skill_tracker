3c0c1fad35a66d53fdf358babfcbcc7c12f58cd7
Emily Boucher <emi.boucher@gmail.com>
2018-06-03 15:44:16 -0400

    add account login links to index posts page, update routes and remove extra user pathways

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :users
+
+  # one user from user pov
+  resource :users, only: %i(new create)
+
+
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
