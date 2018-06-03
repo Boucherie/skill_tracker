cc9a6c2e8c6f94c14a219aabb2faa1e76be9943e
Emily Boucher <emi.boucher@gmail.com>
2018-06-03 15:22:37 -0400

    update authentication for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true
+
+  # Password digest generation (happens automatically as virtual attribute)
+  # def password=(password)
+  #   @password = password
+  #   generate_password_digest(@password)
+  # end
+
 end
