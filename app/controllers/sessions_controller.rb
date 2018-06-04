c10fd430ecd5d5835a74ce100776efa76c8e20b0
Emily Boucher <emi.boucher@gmail.com>
2018-06-03 21:44:13 -0400

    implement session#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    
+
   end
 
   def create
+    u = User.find_by(email: params[:session][:email])
+
+
+
+    if u && u.authenticate(params[:session][:password])
+      # flash[:notice] = 'Login Successful'
+      redirect_to root_url
+    else
+      # flash[:error] = 'Email or Password invalid'
+      render :new
+    end
   end
 
   def destroy
